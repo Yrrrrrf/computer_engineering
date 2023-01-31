40d95a0e64a16b3688f124317ca92b37311edd83
Fernando Reza Campos <fernandorezacampos@gmail.com>
2023-01-30 22:42:47 -0600

    add pandas notebook

---
+++ languages/python/libraries/numpy/numpy_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+arr = np.array([13, 6, 3, 5, 10])
+# print(np.sort(arr))
+# print(np.argmax(arr))
+# print(np.max(arr))
+print(arr.shape)
+np.expand_dims(arr, axis=0)
+print(arr.shape)
+
+# a = np.array([[7, 8], [2, 1], [2, 4]])
+# print(a.shape)
